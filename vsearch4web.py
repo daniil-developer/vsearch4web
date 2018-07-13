7934de4a8c0475e9b52c5b5741ecd7115fb425e6
Dania <boiko.81@list.ru>
2018-07-13 14:03:49 +0300

    Added query viem

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+      print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -10,6 +14,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase,letters))
+    log_request(request, results)
     return render_template('resutls.html',
                             the_phrase = phrase,
                             the_letters = letters,
@@ -21,6 +26,12 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
+@app.route('/viewlog')
+def viem_the_log() -> str:
+    with open('vsearch.log') as log:
+        contens = log.read()
+    return escape(contens) #escape позволяет обойти опасения браузера на <> html теги
 
 if __name__ == '__main__': #если приложение выполняется на локалке, то применить app.run()
     app.run(debug=True)
