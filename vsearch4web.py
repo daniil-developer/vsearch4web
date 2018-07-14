ffdbd1c17bd4e56830da1c59da0705840ce09903
Dania <boiko.81@list.ru>
2018-07-14 17:20:14 +0300

    Database mysql for requests connected

--- vsearch4web.py
+++ vsearch4web.py
@@ -4,9 +4,31 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+import mysql.connector
+
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-      print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    """Журналирует веб-запрос и возвращаемые результаты"""
+    
+    dbconfig = { 'host': '127.0.0.1',
+                 'user': 'vsearch',
+                 'password': 'vsearchpasswd',
+                 'database': 'vsearchlogDB', }
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    
+    _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              values
+              (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
