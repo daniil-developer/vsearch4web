2a5a38bfd949ec26354ff6ad86c4e71d5b5b5312
Dania <boiko.81@list.ru>
2018-07-16 13:25:17 +0300

    Cool data base))

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,37 +1,31 @@
 from flask import Flask, render_template, request, escape
-
 from vsearch import search4letters
+
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
-import mysql.connector
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+                           'user': 'vsearch',
+                           'password': 'vsearchpasswd',
+                           'database': 'vsearchlogDB', }
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Журналирует веб-запрос и возвращаемые результаты"""
-    
-    dbconfig = { 'host': '127.0.0.1',
-                 'user': 'vsearch',
-                 'password': 'vsearchpasswd',
-                 'database': 'vsearchlogDB', }
-
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    
-    _SQL = """insert into log
-              (phrase, letters, ip, browser_string, results)
-              values
-              (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                  (phrase, letters, ip, browser_string, results)
+                  values
+                  (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    """Извлекает данные из запроса; выполняет поиск; возвращает результаты."""
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
@@ -46,18 +40,19 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
+    """Выводит HTML-форму."""
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
 def viem_the_log() -> 'html':
-    contens = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contens.append([])
-            for item in line.split('|'): #разбить строку (по вертикальной черте) а затем обработать каждый эелемент в получаеннмо списке
-                contens[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    """Выводит содержимое файла журнала в виде HTML-таблицы"""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results
+                  from log"""
+        cursor.execute(_SQL)
+        contens = cursor.fetchall() #fetchall возвращает как список кортежей           
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html',
                            the_tltle = 'Viem Log',
                            the_row_titles = titles,
